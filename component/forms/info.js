d42e27bc12c0f3e6cf199d275d064a8357fcace3
Morteza <95617841+pr-morteza@users.noreply.github.com>
2023-02-28 13:03:28 +0330

    Update info.js

--- component/forms/info.js
+++ component/forms/info.js
@@ -2,42 +2,54 @@
 import styles from '../../styles/Home.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Box, FormControl, TextField, Typography } from '@mui/material';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useReducer} from 'react';
 import PeriodContext from '@/context/period';
  
+function reducer(state, action){
+  let pay = action.payload
+  switch (action.type) {
+    case 'name':
+     return  {...state, name:pay, error1: (pay && pay.trim().length>15)} 
+    case 'email':
+      return {...state, email:pay, error2: (pay && !/^\S+@\S+\.\S+$/.test(pay))} 
+    case 'phonenum':
+      return {...state, phoneNum:pay, error3: (pay && (pay.trim().length<(4) || pay.trim().length>(15)))}
+    default:
+      break;
+  }
+}
 
 export default function Info() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phoneNum, setPhoneNum] = useState('')
+  const init = {name:'', email:'', phoneNum:'', errors:{error1:undefined, error2:undefined, error3: undefined}}
   const { setInfoVal} = useContext(PeriodContext)
+  const [state, dispatch] = useReducer(reducer, init)
 
   const nameHandler = (e)=>{
     e.preventDefault();
     let x=e.target.value
-    return  setName(x)
+    dispatch({type:'name', payload:x})
   }
   const emailHandler=(e)=>{
     e.preventDefault();
     let x=e.target.value
-   return  setEmail(x)  
+    dispatch({type:'email', payload:x}) 
   }
   const phoneNumHnadler=(e)=>{
     e.preventDefault();
     let x=e.target.value
-    return  setPhoneNum(x)
+    dispatch({type:'phonenum', payload:x})
   }
+  const {name, email, phoneNum, error1, error2, error3}=state
+
   useEffect(()=>{
-    if(!name||!email ||!phoneNum){
+    if(!name||!email||!phoneNum){
       return setInfoVal(true)
     }
-    ((name &&  name.trim().length>10) ||
-    (email && !/^\S+@\S+\.\S+$/.test(email)) ||
-    (phoneNum && phoneNum.trim().length!==12)) ? setInfoVal(true) : setInfoVal(false)
-  },[name, email, phoneNum])
  
+    (error1|| error2|| error3) ? setInfoVal(true) : setInfoVal(false)
+  },[name, email, phoneNum])
+  
   return (
-
     <>
       <Box>
         <Typography variant='h4' component='h4'>
@@ -57,9 +69,9 @@ export default function Info() {
       >
         <FormControl fullWidth>
           <TextField 
-            error={(name && name.trim().length>10) ? true : false} 
-            value={name} 
+            error={state.error1}  
             onChange={nameHandler} 
+            helperText={state.error1&&'cannot exceed 15 characters'}
             size='small' 
             required 
             placeholder='e.g. Stephen King' 
@@ -68,9 +80,9 @@ export default function Info() {
             variant="outlined" 
           />
           <TextField 
-            error={(email && !/^\S+@\S+\.\S+$/.test(email)) ? true : false} 
-            value={email} 
+            error={state.error2} 
             onChange={emailHandler} 
+            type='email'
             size='small'             
             required 
             placeholder='e.g. stephenking@lorem.com' 
@@ -79,8 +91,7 @@ export default function Info() {
             variant="outlined" 
           />
           <TextField 
-            error={(phoneNum&& phoneNum.trim().length!==12) ? true : false} 
-            value={phoneNum}
+            error={state.error3} 
             inputProps={{type:'number'}} 
             onChange={phoneNumHnadler} 
             size='small' 
@@ -95,5 +106,3 @@ export default function Info() {
     </>
   )
 }
-
-
